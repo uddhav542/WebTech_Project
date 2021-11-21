c84d30ffbcaf68d20cf6c66b3b5197b76f6b39e7
Uddhav Patil <uddhavpatil9560@gmail.com>
2021-11-21 23:45:12 +0530

    changes saved

--- Backend/server.js
+++ Backend/server.js
@@ -44,8 +44,43 @@ const flights = [
   },
   {
       id:5, Departure:'Kerala', Arrival:'Jaipur', Price:'35,868', Duration:'7h30m', Airline:'Go First', DepartureTime:'11:30',ArrivalTime:'19:00'
-  }
-  
+  },
+  {
+      id:6, Departure: 'Jaipur', Arrival:'Kerala', Price:'13,873', Duration:'8h:15min', Airline:'Air Asia', DepartureTime:'11:50', ArrivalTime:'8:05'	
+  },
+  {
+      id:7, Departure: 'Ladakh', Arrival:'Jaipur', Price:'10,491', Duration:'7h:50min', Airline:'Spice Jet', DepartureTime:'12:20', ArrivalTime:'8:10'	
+  },
+  {
+      id:8, Departure: 'Jaipur', Arrival:'Ladkah', Price:'36,980', Duration:'11h:45min', Airline:'Air India', DepartureTime:'8:35', ArrivalTime:'8:20'	
+  },	
+  {
+      id:9, Departure: 'Ladakh', Arrival:'Kerala', Price:'23,408', Duration:'9h:10min', Airline:'Indi-Go', DepartureTime:'1:35', ArrivalTime:'10:45'	
+  },
+  {
+      id:10, Departure: 'Ladakh', Arrival:'Andaman', Price:'40,351', Duration:'44h:10min', Airline:'Air India', DepartureTime:'11:50', ArrivalTime:'8:00'	
+  },
+  {
+      id:11, Departure: 'Andaman', Arrival:'Ladakh', Price:'40,870', Duration:'20h:30min', Airline:'Vistara', DepartureTime:'12:10', ArrivalTime:'8:40'	
+  },
+  {
+      id:12, Departure: 'Kerala', Arrival:'Ladakh', Price:'98,192', Duration:'18h:55min', Airline:'Air India', DepartureTime:'1:25', ArrivalTime:'8:20'	
+  },
+  {
+      id:13, Departure: 'Kerala', Arrival:'Ladakh', Price:'22,289', Duration:'12h:45min', Airline:'Vistara', DepartureTime:'7:55', ArrivalTime:'8:40'	
+  },
+  {
+      id:14, Departure: 'Kerala', Arrival:'Jaipur', Price:'74,476', Duration:'16h:55min', Airline:'Air Asia', DepartureTime:'9:05', ArrivalTime:'2:00'	
+  },
+  {
+      id:15, Departure: 'Jaipur', Arrival:'Andaman', Price:'113,625', Duration:'18h:45min', Airline:'Air India', DepartureTime:'1:30', ArrivalTime:'8:15'	
+  }, 
+  {
+    id:15, Departure: 'Andaman', Arrival:'Kerala', Price:'11,625', Duration:'1h:45min', Airline:'Indigo', DepartureTime:'1:30', ArrivalTime:'15:00'	
+  },
+  {
+    id:15, Departure: 'Jaipur', Arrival:'Ladakh', Price:'13,625', Duration:'8h:45min', Airline:'Air India', DepartureTime:'1:10', ArrivalTime:'9:15'	
+    },  
   ]
 
 
